2392f69ae884a9d02a84c20fd0b5e38d3cd25612
Cedrick Dev <cedrick.develop@gmail.com>
2021-06-22 16:14:36 +0200

    Ajout d'une page Google Connexion

---
+++ app/src/main/java/fr/cedrickdev/homeactivity/GoogleActivity.kt
@@ -0,0 +1,11 @@
+package fr.cedrickdev.homeactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GoogleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_google)
+    }
+}
